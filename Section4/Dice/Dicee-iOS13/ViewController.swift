cbbc3ef1820a2a8e16725b2a63edb2aed1896bc8
shimhyomin <shimhm03@gmail.com>
2021-08-09 23:40:21 +0900

    Add Section4 - Randomise the Dice Images

--- Section4/Dice/Dicee-iOS13/ViewController.swift
+++ Section4/Dice/Dicee-iOS13/ViewController.swift
@@ -14,26 +14,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        // change image using "image Literal"
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-        
-        // "alpha" -> 투명도 조절
-        // diceImageView1.alpha = 0.5
-
-    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//
+//        // change image using "image Literal"
+//        //diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+//        // diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+//
+//        // "alpha" -> 투명도 조절
+//        // diceImageView1.alpha = 0.5
+//
+//    }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         // Button이 눌리면 동작
-        // print on console
         print("Button got tapped.")
-        diceImageView1.image = #imageLiteral(resourceName: "DiceOne")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceOne")
+        
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        
+        // collection of image literal
+        // diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
     }
     
 }
