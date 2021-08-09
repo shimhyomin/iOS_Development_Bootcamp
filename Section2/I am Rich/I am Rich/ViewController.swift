3bb42fa5725bd58103ae309a6be4e4cee16f42a3
shimhyomin <shimhm03@gmail.com>
2021-08-09 19:18:03 +0900

    Add Section2

---
+++ Section2/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by shm on 2021/08/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
