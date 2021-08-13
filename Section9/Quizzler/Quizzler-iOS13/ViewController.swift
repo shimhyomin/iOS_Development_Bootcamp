d08b83b9f4947f8724584cd50fcd072a9a37749c
shimhyomin <shimhm03@gmail.com>
2021-08-13 17:31:28 +0900

    Add Section9 - Quiz App using 2D Array

---
+++ Section9/Quizzler/Quizzler-iOS13/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        ["Four + Tow is equal to Six.", "True"],
+        ["Four - Thress is greater than One.", "True"],
+        ["Three + Eight is less than Ten", "False"]
+    ]
+    
+    var questionNumber = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // view가 load 됨
+        
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!   // Ture or False
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer{
+            print("Right!")
+        } else {
+            print("Wrong!")
+        }
+        
+        if questionNumber + 1 < quiz.count{
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
+    }
+    
+}
+
