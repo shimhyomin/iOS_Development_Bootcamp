2bf3a01428c5f83ce8803f7879c63a3baff9d906
shimhyomin <shimhm03@gmail.com>
2021-08-13 16:49:04 +0900

    Add Section8 - ProgressBar

--- Section8/EggTimer/EggTimer/ViewController.swift
+++ Section8/EggTimer/EggTimer/ViewController.swift
@@ -13,26 +13,21 @@ class ViewController: UIViewController {
 //    let softTime = 5
 //    let mediumTime = 7
 //    let hardTime = 12
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    let eggTimes = ["Soft": 4, "Medium": 10, "Hard": 180]
 
-    var counter = 0
+    var secondsRemaining = 60
+    var timer = Timer()
+    
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    @IBOutlet weak var titleLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
-    }
-
-    @objc func updateCounter() {
-        //example functionality
-        if counter > 0 {
-            print("\(counter) seconds.")
-            counter -= 1
-        }
-    }
+    @IBOutlet weak var progressBar: UIProgressView!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness = sender.currentTitle!
+        titleLabel.text = sender.currentTitle
         // If Else
 //        if hardness == "Soft" {
 //            print(softTime)
@@ -59,7 +54,25 @@ class ViewController: UIViewController {
         
         
         // Countdown Timer
-        counter = eggTimes[hardness]!
+        timer.invalidate()  // timer 초기화해주기! 버튼을 계속 누르면 중복됨
+        // secondsRemaining = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
+        secondsPassed = 0
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            let percentageProgress: Float = Float(secondsPassed) / Float(totalTime)
+            progressBar.progress = percentageProgress
+            
+            secondsPassed += 1
+        } else {
+            timer.invalidate()
+            progressBar.progress = 1
+            titleLabel.text = "Done!"
+        }
     }
     
 }
