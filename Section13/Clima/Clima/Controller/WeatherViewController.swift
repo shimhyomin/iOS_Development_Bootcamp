420c158352c347db7056a403b93887a280b44653
shimhyomin <shimhm03@gmail.com>
2021-08-17 17:26:28 +0900

    Add Section13 - WeatherViewController에 Extension

--- Section13/Clima/Clima/Controller/WeatherViewController.swift
+++ Section13/Clima/Clima/Controller/WeatherViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
+class WeatherViewController: UIViewController {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
@@ -25,7 +25,12 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
         //searchTextField.endEditing(true)
         searchTextField.delegate = self
     }
+}
 
+// MARK: - UITextFielDelegate
+
+extension WeatherViewController: UITextFieldDelegate {
+    
     @IBAction func searchPressed(_ sender: UIButton) {
         searchTextField.endEditing(true)
         print(searchTextField.text!)
@@ -54,7 +59,10 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
         
         searchTextField.text = ""
     }
-    
+}
+
+// MARK: - WeatherManagerDelegate
+extension WeatherViewController: WeatherManagerDelegate {
     func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
         DispatchQueue.main.async {
             self.temperatureLabel.text = weather.temperatureString
@@ -66,4 +74,3 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
         print(error)
     }
 }
-
