9cc29ae4931874a71ffb4e7dd0c2a41f99d3af9c
shimhyomin <shimhm03@gmail.com>
2021-08-21 01:45:25 +0900

    Add Section15 - Study about casting-is and as and as?, as

---
+++ Section15/TypeCasting.playground/Contents.swift
@@ -0,0 +1,62 @@
+import Foundation
+
+class Animal {
+    var name: String
+    init(n: String) {
+        name = n
+    }
+}
+
+class Human: Animal {
+    func code() {
+        print("Typing away...")
+    }
+}
+
+class Fish: Animal {
+    func breatheUnderWater() {
+        print("Breathing under water")
+    }
+}
+
+let hyomin = Human(n: "Shim Hyomin")
+let jack = Human(n: "Jack Bauer")
+let nemo = Fish(n: "Nemo")
+let num = 12
+
+let neighbours = [hyomin, jack, nemo]   // -> Animal Class
+
+// Any
+// 어떤 멤버를 갖든 상관 x
+let anyNeighbours: [Any] = [hyomin, jack, nemo, num]
+
+let neighbour1 = neighbours[0]  // -> Animal Class
+
+// is keyword는 tyep checking
+if neighbours[2] is Human {
+    print("First Neighbour is a Human")
+}
+
+// as! keyword는 Forced Downcast
+func findNemo(from animals: [Animal]) {
+    for animal in animals {
+        if animal is Fish {
+            print(animal.name)
+            //animal.breathUnderWater() -> Error
+            let fish =  animal as! Fish
+            fish.breatheUnderWater()
+        }
+    }
+}
+findNemo(from: neighbours)
+// 잘못 downcast하는 경우
+// let fish = neighbours[1] as! Fish -> Error!
+
+// as? keyword는 downcast
+if let fish = neighbours[1] as? Fish {
+    fish.breatheUnderWater()
+}
+
+// as keyword는 upcast
+let animalFish = nemo as Animal
+// animalFish.breatheUnderWater() -> Error
