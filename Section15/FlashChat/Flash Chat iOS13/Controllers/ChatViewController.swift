15674124dc5579b1a44d8bd581e7bee1d0c78293
shimhyomin <shimhm03@gmail.com>
2021-08-21 18:29:00 +0900

    Add Section15 - Update data using Listener and Sort data and Manage Keyboard using Swift Package Manager

--- Section15/FlashChat/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Section15/FlashChat/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -33,35 +33,39 @@ class ChatViewController: UIViewController {
     }
     
     func loadMessages() {
-        messages = []
-        
-        db.collection(K.FStore.collectionName).getDocuments { querySnapshot, error in
-            if let e = error {
-                print("There was an issue retrieving data from Firestore, \(e)")
-            } else {
-                if let snapshotDocuments = querySnapshot?.documents {
-                    for doc in snapshotDocuments {
-                        let data = doc.data()
-                        if let messageSender = data[K.FStore.senderField] as? String, let messsageBody = data[K.FStore.bodyField] as? String {
-                            let newMesssage = Message(sender: messageSender, body: messsageBody)
-                            self.messages.append(newMesssage)
-                            
-                            DispatchQueue.main.async {
-                                self.tableView.reloadData()
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { querySnapshot, error in
+                
+                self.messages = []
+                
+                if let e = error {
+                    print("There was an issue retrieving data from Firestore, \(e)")
+                } else {
+                    if let snapshotDocuments = querySnapshot?.documents {
+                        for doc in snapshotDocuments {
+                            let data = doc.data()
+                            if let messageSender = data[K.FStore.senderField] as? String, let messsageBody = data[K.FStore.bodyField] as? String {
+                                let newMesssage = Message(sender: messageSender, body: messsageBody)
+                                self.messages.append(newMesssage)
+                                
+                                DispatchQueue.main.async {
+                                    self.tableView.reloadData()
+                                }
                             }
                         }
                     }
                 }
             }
-        }
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
         
         if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
             db.collection(K.FStore.collectionName).addDocument(data: [
-                                                                K.FStore.senderField: messageSender,
-                                                                K.FStore.bodyField: messageBody
+                K.FStore.senderField: messageSender,
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField: Date().timeIntervalSince1970
             ]) { error in
                 if let e = error {
                     print("There was an issue saving data to firestore, \(e)")
