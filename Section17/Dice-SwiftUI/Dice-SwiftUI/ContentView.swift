6c2708db865924719e57e1523001277a1e99d1f0
shimhyomin <shimhm03@gmail.com>
2021-08-24 00:20:46 +0900

    Add Section17 - Dice App using SwiftUI

---
+++ Section17/Dice-SwiftUI/Dice-SwiftUI/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  Dice-SwiftUI
+//
+//  Created by shm on 2021/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack{
+            Image(/*@START_MENU_TOKEN@*/"background"/*@END_MENU_TOKEN@*/)
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("diceeLogo")
+                
+                Spacer()
+                
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+                
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                }
+                .background(Color.red)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct DiceView: View {
+    
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
