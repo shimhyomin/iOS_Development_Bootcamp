d49e565a597c8d8e19b3e7f6f70daeec8bd671ed
shimhyomin <shimhm03@gmail.com>
2021-08-29 17:03:34 +0900

    Add Section19 - Add new Item and tableView.reload

--- Section04/Dice/Dicee-iOS13/ViewController.swift
+++ Section04/Dice/Dicee-iOS13/ViewController.swift
@@ -27,11 +27,13 @@ class ViewController: UIViewController {
 //
 //    }
 
+    
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         // Button이 눌리면 동작
         print("Button got tapped.")
         
         let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        
         
         // collection of image literal
         // diceImageView1.image = diceArray[Int.random(in: 0...5)]
