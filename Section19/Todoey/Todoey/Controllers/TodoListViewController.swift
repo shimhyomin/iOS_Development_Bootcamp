f6e104d9dd07f6502309bb73da19a752682c47f4
shimhyomin <shimhm03@gmail.com>
2021-09-04 20:07:13 +0900

    Add Section19 - Searchbar 검색하고 처음으로 되돌아가기 (DispatchQueue.main.async)

--- Section19/Todoey/Todoey/Controllers/TodoListViewController.swift
+++ Section19/Todoey/Todoey/Controllers/TodoListViewController.swift
@@ -120,5 +120,15 @@ extension TodoListViewController: UISearchBarDelegate {
         
         loadItems(with: request)
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
 }
 
