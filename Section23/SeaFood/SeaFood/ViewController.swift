ba76d7af0edbc9354f06f2f463d28d43d5140326
shimhyomin <shimhm03@gmail.com>
2021-09-12 01:06:03 +0900

    Add Section22 - Add CoreML(Classification hotdog)

--- Section23/SeaFood/SeaFood/ViewController.swift
+++ Section23/SeaFood/SeaFood/ViewController.swift
@@ -35,13 +35,40 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 fatalError("Could not convert UIImage into CIImage.")
             }
             
-            
+            detect(image: ciimage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
     }
     
-
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreMS Model Failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed process image.")
+            }
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try! handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         
